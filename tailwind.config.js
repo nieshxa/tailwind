15571e59185642dd0b5aabc723d682ec4fdb031c
nieshxa <niknakriego@gmail.com>
2023-10-08 14:30:17 +0800

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        midnight: "#14213d",
+        darktangerine: "#FCA311",
+        midnight: "#14213d",
+      },
+      fontFamily: {
+        paytone: ['"Paytone One"', "sans-serif"],
+        questrial: ['"Questrial"', "sans-serif"],
+      },
+    },
+  },
+};
